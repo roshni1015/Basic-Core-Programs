e34818abf51f697a35d09533e90cbcd3563951cf
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-24 01:21:41 +0530

    Factors

---
+++ Basic Program/Basic Program/Factors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Program
+{
+    internal class Factors
+    {
+        public void factors()
+        {
+            int n, i;
+            Console.WriteLine("Enter the Number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Factors:", a);
+            for (i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
