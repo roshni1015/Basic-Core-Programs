153c026a4ee05c5d6b1c66c130c244db142f718f
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-23 23:26:22 +0530

    Flipcoin

---
+++ Basic Program/Basic Program/Flipcoin.cs
@@ -0,0 +1,50 @@
+﻿namespace Basic_Program
+{
+    internal class Flipcoin
+    {
+        public int filpcoin()
+        {
+            Console.WriteLine("Let's flip a coin 1000 times");
+            Random Rand = new Random();
+            int heads = 0;
+            int tails = 0;
+            int result = 0;
+
+            for (int i = 0; i< 1000; i++)
+
+            {
+
+                result = Rand.Next(0, 2);
+
+ 
+
+                if (result == 1)
+
+                {
+
+                    heads++;
+
+                }
+
+                else
+
+                {
+
+                    tails++;
+
+                }
+
+
+
+
+            }
+
+            Console.WriteLine("heads was flipped {0} times", heads);
+
+            Console.WriteLine("tails was flipped {0} times", tails);
+
+            return result;
+       }
+
+    }
+}
--- Basic Program/Basic Program/Program.cs
+++ Basic Program/Basic Program/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Basic Core Pogram!");
 while (true)
 {
 
-    Console.WriteLine("Choose the Program to be executed :\n1)Swapingnumber\n2)QuotientRemainder\n3)Largestnumber\n4)VowelConsonant\n5)Evenodd");
+    Console.WriteLine("Choose the Program to be executed :\n1)Swapingnumber\n2)QuotientRemainder\n3)Largestnumber\n4)VowelConsonant\n5)Evenodd\n6)Flipcoin");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -28,6 +28,10 @@ while (true)
         case 5:
             Evenodd evenodd = new Evenodd();
             evenodd.evenodd();
+            break;
+        case 6:
+            Flipcoin filpcoin = new Flipcoin();
+            filpcoin.filpcoin();
             break;
         default:
           break;
