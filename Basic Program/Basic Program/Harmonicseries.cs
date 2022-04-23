3cfc96e1881d862426486a515fee327e2bd3e8ab
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-24 01:12:03 +0530

    Harmonicseries

---
+++ Basic Program/Basic Program/Harmonicseries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Program
+{
+    internal class Harmonicseries
+    {
+        public void HarmonicSeries()
+        {
+            int i, s;
+            int n = 0;
+            Console.WriteLine("Enter value of N : ");
+            s = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= s; i++)
+            {
+                Console.Write(" 1/{0}+", i);
+                s += 1 / i;
+            }
+            Console.WriteLine("\nSum of Series upto {0} terms : {1} ", n, s);
+        }
+    }
+}
--- Basic Program/Basic Program/Program.cs
+++ Basic Program/Basic Program/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Basic Core Pogram!");
 while (true)
 {
 
-    Console.WriteLine("Choose the Program to be executed :\n1)Swapingnumber\n2)QuotientRemainder\n3)Largestnumber\n4)VowelConsonant\n5)Evenodd\n6)Flipcoin\n7)Leapyear\n8)Powerof2");
+    Console.WriteLine("Choose the Program to be executed :\n1)Swapingnumber\n2)QuotientRemainder\n3)Largestnumber\n4)VowelConsonant\n5)Evenodd\n6)Flipcoin\n7)Leapyear\n8)Powerof2\n9)HarmonicSeries");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -40,6 +40,10 @@ while (true)
         case 8:
             Powerof2 power = new Powerof2();
             power.powerof2();
+            break;
+        case 9:
+            Harmonicseries HarmonicSeries = new Harmonicseries();
+            HarmonicSeries.HarmonicSeries();
             break;
         default:
           break;
