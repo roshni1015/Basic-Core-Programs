2fedf1aa86a2c16cffa800adc0af9ca656f0c315
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-23 22:10:56 +0530

    Swap Number

--- Basic Program/Basic Program/Program.cs
+++ Basic Program/Basic Program/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Basic_Core_Programming;
+
 Console.WriteLine("Basic Core Pogram!");
+while (true)
+{
+
+    Console.WriteLine("Choose the Program to be executed :\n1)Swapingnumber");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+          swapingnumber swap = new swapingnumber();
+          swap.swap();
+          break;
+        default:
+          break;
+
+
+
+            }
+    }
---
+++ Basic Program/Basic Program/swapingnumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Programming
+{
+    internal class swapingnumber
+    {
+        public void swap()
+        {
+            int a, b;
+            Console.WriteLine("Enter the number a :");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number b :");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before swaping");
+            Console.WriteLine("a =" + a);
+            Console.WriteLine("b =" + b);
+
+            Console.WriteLine("After swaping");
+            Console.WriteLine("a =" + b);
+            Console.WriteLine("b =" + a);
+
+
+        }
+    }
+}
