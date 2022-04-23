fc3df00469551327f59524d06084109fd1d7e453
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-23 22:26:19 +0530

    Largestnumber

---
+++ Basic Program/Basic Program/Largestnumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Program
+{
+    internal class Largestnumber
+    {
+        public void largenumber()
+        {
+            int n1, n2, n3;
+            Console.WriteLine("*********Find the largest of three numbers**********");
+            Console.WriteLine("Enter the 1st number n1 :");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the  2nd number n2:");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 3rd  number n3 :");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 > n2 && n1 > n3)
+            {
+
+                Console.WriteLine("n1 is the greatest among three");
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                Console.WriteLine("n2 is the greatest among three");
+            }
+            else
+            {
+                Console.WriteLine("n3 is the greatest among three");
+
+            }
+
+        }
+    }
+}
--- Basic Program/Basic Program/Program.cs
+++ Basic Program/Basic Program/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine("Basic Core Pogram!");
 while (true)
 {
 
-    Console.WriteLine("Choose the Program to be executed :\n1)Swapingnumber\n2)QuotientRemainder");
+    Console.WriteLine("Choose the Program to be executed :\n1)Swapingnumber\n2)QuotientRemainder\n3)Largestnumber");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -16,6 +16,10 @@ while (true)
         case 2:
             QuotientRemainder quotientRemainder = new QuotientRemainder();
             quotientRemainder.quotientRemainder();
+            break;
+        case 3:
+            Largestnumber large = new Largestnumber();
+            large.largenumber();
             break;
         default:
           break;
